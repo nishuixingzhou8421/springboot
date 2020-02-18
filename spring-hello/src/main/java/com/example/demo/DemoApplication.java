5f25ae230167d176ca7cc9b7e598048996682cae
mzw <316483791@qq.com>
2020-02-18 21:37:21 +0800

    ok

--- spring-hello/src/main/java/com/example/demo/DemoApplication.java
+++ spring-hello/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println(1111);
-
         SpringApplication.run(DemoApplication.class, args);
     }
 
